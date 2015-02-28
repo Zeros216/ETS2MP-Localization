fbe66a9d2c70b1e218f1beed67a98543075eedf0
Zeros216 <pdrfixgr@gmail.com>
2015-02-28 09:45:38 +0200

    Create greek.php

---
+++ greek.php
@@ -0,0 +1,108 @@
+<?php
+//
+// ETS2MP Homepage language file.
+// File: greek.php
+// Language name: Greek
+// Translated name: Greek
+//
+$_lang["download"] = "Λήψη";
+$_lang["home"] = "Αρχική";
+$_lang["status"] = "Κατάσταση";
+$_lang["username"] = "'Ονομα χρήστη";
+$_lang["password"] = "Κωδικός";
+$_lang["login"] = "Σύνδεση";
+$_lang["register"] = "Εγγραφή";
+$_lang["download.latest.version"] = "Λήψη τελεύταιας έκδοσης";
+$_lang["current.release"] = "Παρούσα έκδοση:";
+$_lang["language"] = "Γλώσσα";
+$_lang["join"] = "ΛΑΒΕΤΕ ΜΕΡΟΣ";
+$_lang["mainpage.join"] = "Ενωθήτε με εκατοντάδες θαυμαστες φορτηγών ζωντανά!";
+$_lang["ts3.advertisement"] = "Συνομιλήστε ζωντανά με αλλους οδηγούς - Κάντε κλικ για να συνδεθήτε με τον TS3 server.";
+$_lang["facebook.advertisement"] = "Δείτε τα νέα μας στο facebook.";
+$_lang["registration.blocked"] = "Η εγγραφή είναι μπλοκαρισμένη - επεξεργαζόμαστε τα στοιχεία που μπλοκάρουν την εγγραφή.Η λειτουργία θα είναι διαθέσιμη όταν ολοκληρωθούν οι εργασίες και θα είμαστε σε θέση να την δημοσιοποιήσουμε στην νέα έκδοση.";
+$_lang["invalid.username.password"] = "Μη έγκυρο mail ή κωδικός.";
+$_lang["already.logged"] = "Είστε ήδη συνδεμένος-η.";
+$_lang["welcome"] = "Καλωσορισατε";
+$_lang["logout"] = "Αποσύνδεση";
+$_lang["bans"] = "Bans";
+$_lang["version"] = "Έκδοση";
+$_lang["update.time"] = "Χρόνος ενημέρωσης";
+$_lang["req.game.version"] = "Απαιτούμενη έκδοση παιχνιδιού";
+$_lang["requires"] = "Απαιτήσεις";
+$_lang["or.newer"] = "ή νεώτερο";
+$_lang["game.client"] = "Εξυπηρετητής παιχνιδιού";
+$_lang["download.no.permissions"] = "Για να κατεβάσετε το mod πρέπει να είστε συνδεμένος/η.";
+$_lang["why.create.account"] = "Γιατί να δημιουργήσω λογαριασμό?<br />Ένας λογ/μος θα είναι σε χρήση για να συνδέεστε σε όλες τις υπηρεσίες(φόρουμ,παιχνίδι,οικονομία,κτλ)Σχεδιάζουμε να τα χρησιμοποιήσουμε όταν θα εισάγουμε οικονομικά και ιδιωτικά στατιστικά δεδομενα των οδηγών στο προσεχές μέλλον.Θα μπορείτε να επεξεργάζεστε το δικό σας προφίλ.";
+$_lang["website.title"] = "ETS2MP - Εικονικός κόσμος των φορτηγών - multiplayer mod for Euro Truck Simulator 2 game by SCS Software";
+$_lang["cookies.warning"] = "Αυτή η σελίδα χρησιμοποιεί <a href=\"http://en.wikipedia.org/wiki/HTTP_cookie\">cookies</a>. Συνεχίζοντας την πλοήγηση σας σε αυτήν τη σελίδα,συμφωνείτε με τους όρους χρήσης των cookies.";
+$_lang["forum"] = "Φόρουμ";
+$_lang["cannot.create.account.when.logged"] = "Δεν μπορείτε να δημιουργήσετε νεο λογ/μος εφόσον είστε ήδη συνδεμένος/η.";
+$_lang["successfully.registered"] = "Η εγγραφή σας ειναι επιτυχής.Παρακαλούμε,ελέγξτε το email σας για να ενεργοποιήσετε τον λογαριασμό σας";
+$_lang["error.unknown"] = "Άγνωστο σφάλμα";
+$_lang["credentials.taken"] = "Το ψευδώνυμο,email ή steamid είναι ήδη καταχωρημένο";
+$_lang["error.no.steam.data"] = "Για να δημιουργήσετε λογ/μο στο ΕΤS2MP πρέπει να έχετε καταχωρημένο το παιχνίδι ΕΤS2 μέσω του λογ/μου σας στο steam.Εφόσον το εχετε στην βιβλιοθήκη του steam,ελέγξτε αν το προφίλ σας ειναι σε ¨δημόσια" λειτουργία.Αν το έχετε σε "κρυφή" λειτουργία,γυρίστε το σε "δημόσια".";
+$_lang["register.steam.info"] = "Για να κάνετε εγγραφή πρέπει πρώτα να συνδεθήτε με τον λογ/μο σας στο Steam.Πατήστε το κουμπί κάτω για να μεταφερθήτε στην σελίδα επαλήθευσης του Steam.";
+$_lang["rules.info"] = "Όταν πατήσετε το κουμπί της εγγραφής,αυτομάτος αποδέχεστε τους όρους της κοινότητας μας.Για να δείτε τους όρους,πατήστε εδώ.";
+$_lang["mail"] = "E-Mail";
+$_lang["repeat.mail"] = "Επανάληψη email";
+$_lang["new.password"] = "Νέος κωδικός";
+$_lang["repeat.new.password"] = "Επανάληψη νέου κωδικού";
+$_lang["new.password.summary"] = "Αν αφήσετε αυτά τα πεδία κενά,ο κωδικός δεν θα αλλάξει.";
+$_lang["old.password"] = "Παλιός κωδικός";
+$_lang["repeat.old.password"] = "Επανάληψη παλίου κωδικού";
+$_lang["save.account.data"] = "Αποθήκευση δεδομένων λογ/μου";
+$_lang["edit.account"] = "Επεξεργασία λογ/μου";
+$_lang["forgot.password"] = "Ξεχάσατε τον κωδικό? Πατήστε <a href=\"index.php?page=resetPassword\">here</a>.";
+$_lang["forgot.password.summary"] = "Μήπως ξεχάσατε τον κωδικό? Γράψτε απο κάτω το mail σας και πατήστε \"Reminder password\" button.";
+$_lang["reminder.button"] = "Υπενθυμιση κωδικού";
+$_lang["reset.password"] = "Επαναφόρα κωδικού";
+$_lang["password.reminder.mail.title"] = "Αίτημα υπενθύμισης κωδικού.";
+$_lang["password.reminder.content"] = "Γειά σας.Κάποιος\ια από την ip {0} ζητάει την επαναφορά κωδικού του λογ/μου στο ETS2MP.Για την επαναφορά του κωδικού σας πατήστε σε αυτόν τον σύνδεσμο: {1}.\n αν δεν σας αφορά αυτό το mail αγνοήστε το.\n\nΜε εκτίμηση\nETS2MP Team";
+$_lang["new.password.mail.title"] = "Ο νέος σας κωδικός.";
+$_lang["new.password.mail.content"] = "Γειά σας.\n\nΕίναι ο νέος σας κωδικός: {0}\n\nΑλλάξτε τον αμέσως μόλις συνδεθήτε.\n\nΜε εκτίμηση\nETS2MP Team";
+$_lang["account.activated"] = "Ο λογ\μος σας ενεργοποιήθηκε επιτυχώς.";
+$_lang["unable.to.activate"] = "Αδυναμία ενεργοποίησης λογ/μου, Λανθασμένο κλειδί ή ο λογ/μος είναι ήδη ενεργός.";
+$_lang["not.activated"] = "Ο λογ/μος σας δεν ενεργοποιήθηκε ακόμα. Αν δεν έχετε λάβει το μήνυμα ενεργοποίησης click εδώ <a href=\"index.php?action=resendActivation\">here</a> για να σας αποσταλεί ξανά το κλειδί ενεργοποίησης.";
+$_lang["force.password.change"] = "Παρακαλώ αλλάξτε τον κωδικό σας αμέσως. Click εδώ <a href=\"index.php?page=editAccount\">here</a> για να το κάνετε.";
+$_lang["forgot.password.link.sent"] = "Ελεγξετε το e-mail σας. Σας έχουμε στείλει τον σύνδεσμο επαναφοράς κωδικού.";
+$_lang["forgot.password.sent"] = "Ελεγξετε το e-mail σας. Σας έχουμε στείλει τον καινούργιο κωδικό.";
+$_lang["forgot.password.invalid.key"] = "Μη έγκυρος κωδικός κλειδιού επαναφοράς.";
+$_lang["activation.key.resent"] = "Καινούργιο κλειδί ενεργοποίησης εχει σταλεί στο e-mail σας.";
+$_lang["activation.mail.title"] = "ETS2MP Ενεργοποίηση λογαριασμου.";
+$_lang["activation.mail.content"] = "Γειά σας.Αυτό είναι το κλειδί ενεργοποίησης σας: {0}\n\nGreetings\nETS2MP Team";
+$_lang["no.login"] = "Πρέπει να συνδεθήτε για να έχετε πρόσβαση σε αυτήν τη σελίδα.";
+$_lang["settings.saved.with.mail"] = "Ο λογ/μος σας έχει αποθηκευτή. Ελέγξετε το νέο σας e-mail και ενεργοποίηστε τον λογ/μο σας ξανά.";
+$_lang["new.email.in.use"] = "Αυτο το email βρίσκετε ήδη σε χρήση.";
+$_lang["email.does.not.match"] = "Το emails που χρησιμοποιήτε δεν ταιριάζει.";
+$_lang["invalid.email.format"] = "Το E-Mail που γράψατε δεν είναι έγκυρο.";
+$_lang["new.password.does.not.match"] = "Ο νέος κωδικός που γράψατε δεν ταιριάζει.";
+$_lang["too.short.password"] = "Ο νέος κωδικός έχει λίγους χαρακτήρες. Ελάχιστη: 5 χαρακτήρες";
+$_lang["old.password.does.not.match"] = "Ο παλιός κωδικός δεν ταιριάζει.";
+$_lang["settings.saved"] = "Ο λογ/μος σας έχει αποθηκευτή.";
+$_lang["invalid.old.password"] = "Ο παλιος κωδικός δεν είναι έγκυρος, αδυναμία αποθήκευσης λογαριασμού.";
+$_lang["no.password.change.forced"] = "Για να αλλάξετε κωδικό πρέπει πρωτα να αποσυνδεθήτε.";
+$_lang["password.reminder.edit.account"] = "Δεν θυμάστε τον κωδικό σας? Click εδώ <a href=\"index.php?page=resetPassword\">here</a>.";
+$_lang["contact"] = "Επικοινωνία";
+$_lang["your.mail"] = "Το e-mail σας";
+$_lang["content"] = "Περιεχόμενο";
+$_lang["send"] = "Αποστολή";
+$_lang["contact.message.sent"] = "Σας ευχαριστούμε για την επικοινωνία.Παρακαλώ περιμένετε την απάντηση μας.";
+$_lang["topic"] = "Θέμα";
+$_lang["contact.message.too.short"] = "Το μήνυμα ειναι μικρό. Ελάχιστο 10 χαρακτήρες.";
+$_lang["contact.topic.too.short"] = "Ο τίτλος του θέματος ειναι μικρός. Ελάχιστο 10 χαρακτήρες.";
+$_lang["invalid.mail.forgot.password"] = "Αδυναμία έυρεσης αυτού το λογ/μου σε αυτό το e-mail.";
+$_lang["error.cannot.fetch.steam.api"] = "Αδυναμία σύνδεσης του steam προφίλ σας στο steam. Δοκιμάστε αργότερα.";
+
+    Status
+    API
+    Training
+    Shop
+    Blog
+    About
+
+    © 2014 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Contact
+
